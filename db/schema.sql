49ef0dc5af8e6d12562c5ad276825019fc5df4ac
BluSkreen <jasonduran303@gmail.com>
2022-12-08 19:09:41 -0700

    file setup, models and seeds working. Working on routes for CRUD operations.

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+-- DROP DATABASE
+DROP DATABASE IF EXISTS ecommerce_db;
+
+-- CREATE DATABASE
+CREATE DATABASE ecommerce_db;
+
+USE ecommerce_db;
+SELECT * FROM category;
+SELECT * FROM product;
+SELECT * FROM product_tag;
+SELECT * FROM tag
